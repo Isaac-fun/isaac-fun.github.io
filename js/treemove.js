d9df20936e1842b6739afa84b448675172b66f01
Isaac Moore <totally.isaac123@gmail.com>
2026-07-31 00:38:00 -0400

    touchup
    added parallax mouse movement to home screen and updated mobile view

---
+++ js/treemove.js
@@ -0,0 +1,9 @@
+window.addEventListener('mousemove', parallax);
+function parallax(e){
+    document.querySelectorAll('.object').forEach(function(move){
+        let moving_value = move.getAttribute('data-value');
+        let x = (e.clientX * moving_value) /250 ;
+        let y = (e.clientY * moving_value) /250 ;
+        move.style.transform = `translate(${x}px, ${y}px)`;
+    });
+}
